22ea5984d3a34f101ca666891ac4813ee75ef3c1
endrewmota <endrew.desousa@gmail.com>
2022-08-13 15:38:05 -0400

    Adicionando menu e botao de cadastro de produtos

--- Sistema/Form1.Designer.cs
+++ Sistema/Form1.Designer.cs
@@ -28,12 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.cadastrosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.produtosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.categoriasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btn_cad_produtos = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cadastrosToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // cadastrosToolStripMenuItem
+            // 
+            this.cadastrosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.produtosToolStripMenuItem,
+            this.categoriasToolStripMenuItem});
+            this.cadastrosToolStripMenuItem.Name = "cadastrosToolStripMenuItem";
+            this.cadastrosToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.cadastrosToolStripMenuItem.Text = "Cadastros";
+            // 
+            // produtosToolStripMenuItem
+            // 
+            this.produtosToolStripMenuItem.Name = "produtosToolStripMenuItem";
+            this.produtosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.produtosToolStripMenuItem.Text = "Produtos";
+            // 
+            // categoriasToolStripMenuItem
+            // 
+            this.categoriasToolStripMenuItem.Name = "categoriasToolStripMenuItem";
+            this.categoriasToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.categoriasToolStripMenuItem.Text = "Categorias";
+            // 
+            // btn_cad_produtos
+            // 
+            this.btn_cad_produtos.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_cad_produtos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_cad_produtos.ForeColor = System.Drawing.Color.DimGray;
+            this.btn_cad_produtos.Image = global::Sistema.Properties.Resources.cad_produtos_40x40;
+            this.btn_cad_produtos.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_cad_produtos.Location = new System.Drawing.Point(35, 83);
+            this.btn_cad_produtos.Name = "btn_cad_produtos";
+            this.btn_cad_produtos.Size = new System.Drawing.Size(95, 83);
+            this.btn_cad_produtos.TabIndex = 1;
+            this.btn_cad_produtos.Text = "Cadastrar Produtos";
+            this.btn_cad_produtos.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_cad_produtos.UseVisualStyleBackColor = true;
+            this.btn_cad_produtos.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_cad_produtos);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem cadastrosToolStripMenuItem;
+        private ToolStripMenuItem produtosToolStripMenuItem;
+        private ToolStripMenuItem categoriasToolStripMenuItem;
+        private Button btn_cad_produtos;
     }
 }
--- Sistema/Form1.cs
+++ Sistema/Form1.cs
@@ -6,5 +6,10 @@ namespace Sistema
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
